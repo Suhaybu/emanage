549215d74194afbf17c2e25763cb1522d9c93612
Muhammad Suhayb Ullah <suhaybullah@icloud.com>
2022-10-29 00:55:45 +0300

    Update2

---
+++ src/main/java/com/xyzsoft/emanage/Column.java
@@ -0,0 +1,17 @@
+package com.xyzsoft.emanage;
+
+public class Column {
+    private String columnName;
+
+    public Column(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+}
---
+++ src/main/java/com/xyzsoft/emanage/IntegerColumn.java
@@ -0,0 +1,11 @@
+package com.xyzsoft.emanage;
+
+public class IntegerColumn extends Column {
+
+    private List<Integer> data;
+    public IntegerColumn(String columnName) {
+        su per(columnName);
+     
+
+    
+}
