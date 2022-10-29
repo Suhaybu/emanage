859e06bc732934996e548509ace85c6eaadb925f
AliAlYousef70 <98760997+AliAlYousef70@users.noreply.github.com>
2022-10-29 14:07:00 +0300

    Add files via upload

---
+++ src/main/java/com/xyzsoft/emanage/xIsChangeCheck.java
@@ -0,0 +1,18 @@
+package swe;
+
+
+public class xIsChangeCheck {
+	
+	public boolean changeCheck (int newValue, int oldValue)  {
+		if (newValue > oldValue) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+	
+
+}
